e31b743b5be97a36da847e7ff52cda6556339821
Afterburn <mujahideen.solutions@gmail.com>
2015-01-30 23:42:05 -0700

    comments

--- src/sketch.ino
+++ src/sketch.ino
@@ -3,13 +3,17 @@ int volt_div_pin  = A0;
 int relay_pin     = 12;
 int aux_pin       = 11;
 int aux_pin_state = 0;
-unsigned long  start_time = 0;
 int now           = 0;
 int voltage_warn  = 0;
-float low_voltage   = 12.00;
-float voltage     = 0;
-float raw_input   = 0;
 int last_voltage  = -1;
+int wait_time     = 30;
+
+unsigned long  start_time = 0;
+
+// Based on http://www.energymatters.com.au/components/battery-voltage-discharge/ 
+float low_voltage   = 12.20;
+float voltage     = 0;
+
 
 void wait(int seconds)
 {
@@ -95,12 +99,15 @@ void loop()
 {
 
     float avg_voltage = 0;
+    float raw_input   = 0;
     
     relay_on(relay_pin);
     
     raw_input = analogRead(volt_div_pin);
     voltage = ((raw_input/1024) * 5)/.32;
 
+    /* Basically this triple checks that the voltage is low so we don't sit there  
+       and flip the relay on and off just because it had an incorrect reading */  
     if (voltage < low_voltage)
     {
             if (voltage_warn < 3)
@@ -118,40 +125,43 @@ void loop()
         }
 
     }
-
+    /* Ensure the timer stays at 0 because we are still low on voltage */
     if (voltage_warn == 3)
     {
         low_voltage_routine();
         reset_timer();
     }
 
+    /* If the voltage is back to normal see if the aux_pin is off. If it 
+       is off, wait 5 minutes to make sure, then turn on the relay. */
     else if (voltage_warn == 0)
     {
         if (aux_pin_state == 0 && start_time == 0)
         {
             start_timer();
         }
-        
+
         else if (aux_pin_state == 0 && start_time != 0 && elapsed_time() >= 300000)
         {
             nominal_voltage_routine();
             reset_timer();
         }
     }
-
+    
+    /* Turn voltage into an int */
     last_voltage = voltage * 100;
 
+    
     relay_off(relay_pin);
 
+    /* Output the voltage in an easy format */
     Serial.print(last_voltage);
     Serial.print(":");
     Serial.print(voltage_warn);
     Serial.print(":");
     Serial.println(aux_pin_state);
-    //Serial.print(":");
-    //Serial.println(elapsed_time());
     Serial.flush();
 
 
-    wait(30); 
+    wait(wait_time); 
 }
