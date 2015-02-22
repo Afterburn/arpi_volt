532fb24872bf7571f800c493c603b39fed72487f
Distance <mujahideen.solutions@gmail.com>
2015-02-21 22:32:58 -0700

    emailtools.py

---
+++ emailtools.py
@@ -0,0 +1,33 @@
+import subprocess
+import smtplib
+import json
+from email.mime.text import MIMEText
+
+
+class EmailTools():
+    def __init__(self, config):
+        self.config = config
+
+
+    def send_email(self, to, subject, body):
+        msg = MIMEText(body)
+        msg['Subject']    = subject
+        msg['From']       = self.config['email']['from']
+        msg['To']         = to
+        msg['Body']       = body
+        msg['Precedence'] = 'bulk'
+        msg['Auto-Submitted'] = 'auto-generated'
+        s = smtplib.SMTP('localhost')
+        s.sendmail(msg['From'], msg['To'], msg.as_string())
+        s.quit()
+
+
+
+
+
+with open('config', 'r') as cfg:
+    config = json.loads(cfg.read())
+
+e = EmailTools(config)
+e.send_email(config['email']['to'], 'RP up', 'some message')
+
