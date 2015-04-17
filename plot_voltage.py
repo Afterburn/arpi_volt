8c68c74f21e5840d4b253ef0ab0cc7b04edf4583
Distance <mujahideen.solutions@gmail.com>
2015-04-17 16:45:36 -0600

    plotting settings

--- plot_voltage.py
+++ plot_voltage.py
@@ -7,7 +7,7 @@ from plotly.graph_objs import *
 
 from sql.db_connect import Connect
 
-db_uri  = 'mysql+mysqldb://root@localhost/stats'
+db_uri  = 'mysql+mysqldb://root:password@localhost/stats'
 db      = Connect(db_uri)
 
 time_list    = []
@@ -18,7 +18,10 @@ seconds = timedelta(seconds=130)
 #query = db.session.query(db.base.classes.rss.title, db.base.classes.rss.id).filter_by(title=title).first()
 query = db.session.query(db.base.classes.stats).filter(db.base.classes.stats.time >= '2015-01-01')
 
+print('Entries: %s' % (query.count()))
+
 for q in query:
+    print(q.id)
    
     future_query = db.session.query(db.base.classes.stats).filter_by(id=q.id + 1).first()
 
@@ -42,7 +45,19 @@ for q in query:
     voltage_list.append(q.voltage)
 
 
-data = Data([Scatter(x=time_list,y=voltage_list)])
+data   = Data([Scatter(x=time_list,y=voltage_list)])
+layout = Layout(
+        autosize=False,
+        width=1366,
+        height=300,
+        margin=Margin(
+            l=80,
+            r=80,
+            b=100,
+            t=100
+            pad=1
+            ),
+        )
 
-
-plot_url = py.plot(data, filename='rpi-voltage')
+fig = Figure(data=data, layout=layout)
+plot_url = py.plot(fig, filename='rpi-voltage')
